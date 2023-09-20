f780ad8d70a5b0e31b9794b57e89f79cd58a5ecf
unknown <mohamedkholeef082@gmail.com>
2023-09-20 19:02:48 +0300

    post_type feature

--- posts/models.py
+++ posts/models.py
@@ -5,11 +5,22 @@ from django.utils import timezone
 
 class Post(models.Model):
     
+    TYPE_CHOICES = [
+    ('political', 'political'),
+    ('scientific', 'scientific'),
+    ('literary', 'literary'),
+    ('historical', 'historical'),
+    ('philosophical', 'philosophical'),
+    ('personal', 'personal'),
+    ('other', 'other'),
+]
+        
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE, related_name='posts')
     title = models.CharField(max_length=255)
     description = models.TextField()
     img = models.ImageField(upload_to="uploads/%Y/%m/%d/", blank=True)
     date = models.DateTimeField(default=timezone.now)
+    post_type = models.CharField(max_length=50, choices=TYPE_CHOICES, default='other')
     
     def __str__(self) -> str:
         return self.title
--- posts/serializers.py
+++ posts/serializers.py
@@ -6,12 +6,19 @@ from .models import Post
 class PostSerializer(serializers.ModelSerializer):
     
     user = serializers.HiddenField(default=serializers.CurrentUserDefault())
+    post_type = serializers.SerializerMethodField()
     
     class Meta:
         model = Post
-        fields = ['id', 'user', 'title', 'description', 'img', 'date']
+        fields = ['id', 'user', 'title', 'description', 'img', 'date', 'post_type', 'post_type_val']
         extra_kwargs = {
             'date': {'read_only': True},
+            'post_type_val': {'source': 'post_type', 'write_only': True}
         }
 
+    
+    def get_post_type(self, obj):
+        
+        post_type = obj.post_type
+        return {'value': post_type,'label': post_type}
 
